c55d4c71db1ab558626c5270de626a5f12c4dcff
eladbash <elad.bash@gmail.com>
2020-02-19 02:16:37 +0200

    remove build files from git

--- API/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-var port = 3000;
-app.get('*', function (req, res) {
-    res.send("Called URL " + req.url);
-});
-app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
